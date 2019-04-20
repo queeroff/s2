3d6d2522fa4e65c50660ffd01c9ad98f9d2f26e4
queeroff <39581460+queeroff@users.noreply.github.com>
2019-04-20 17:31:51 -0500

    Add files via upload

---
+++ vote/js/index.js
@@ -0,0 +1,27 @@
+let alta = document.getElementById('alta');
+let background = document.getElementById('background');
+let title = document.getElementById('title');
+
+function rosegoldColor() {
+  alta.src='http://www.thequeeroff.com/images/season2/kitty_pz.png';
+  background.style.backgroundColor = '#f44242';
+  title.style.color= '#f44242';
+}
+
+function coralColor() {
+  alta.src='http://www.thequeeroff.com/images/season2/porter_pz.png';
+  background.style.backgroundColor = '#f48c41';
+  title.style.color= '#f48c41';
+}
+
+function fuschiaColor() {
+  alta.src='http://www.thequeeroff.com/images/season2/blue_pz.png';
+  background.style.backgroundColor = '#41f49d';
+  title.style.color= '#41f49d';
+}
+
+function goldColor() {
+  alta.src= 'http://www.thequeeroff.com/images/season2/Bearded_pz.png';
+  background.style.backgroundColor= '#4158f4';
+  title.style.color= '#4158f4';
+}
